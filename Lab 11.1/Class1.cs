028d474ce4d5030f84ce6de03be63062cb67cca1
krist <krist@LAPTOP-HIQLHLL6>
2022-11-24 15:59:37 +0500

    Добавьте файлы проекта.

---
+++ Lab 11.1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_11
+{
+    struct Pryamaya
+    {
+        double k;
+        double b;
+        public Pryamaya(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0)
+            {
+                return ("Решений нет");
+            }
+            double r = -b / k;
+            return $"Решение-{r}";
+
+        }
+    }
+}
---
+++ Lab 11.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Pryamaya pryamaya = new Pryamaya(k, b);
+            Console.WriteLine(pryamaya.Root());
+            Console.ReadKey();
+        }
+    }
+}
